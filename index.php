a78e56e14dff0d0dacc3fe3ccca596eb2d6edda1
shijianhang <3076679995@qq.com>
2016-10-06 00:20:47 +0800

    添加顶级目录的定义

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+
+// 定义相关目录的常量
+define('ROOTPATH', dirname(__FILE__).DIRECTORY_SEPARATOR);
+define('APPPATH', ROOTPATH.'application'.DIRECTORY_SEPARATOR);
+define('SYSPATH', ROOTPATH.'system'.DIRECTORY_SEPARATOR);
+
+
