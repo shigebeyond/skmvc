a197c8960dcf3a22eaf1f253d49a3f6393778c21
shijianhang <3076679995@qq.com>
2016-10-08 01:44:14 +0800

    入口文件调用server来处理请求

--- index.php
+++ index.php
@@ -6,6 +6,16 @@ define('APPPATH', ROOTPATH.'application'.DIRECTORY_SEPARATOR);
 define('SYSPATH', ROOTPATH.'system'.DIRECTORY_SEPARATOR);
 
 // 自动加载
+require SYSPATH.'classes/Sk/Loader.php';
 spl_autoload_register(array('Sk_Loader', 'load_class'));
 
-
+if (PHP_SAPI == 'cli') // 命令行
+{
+	// TODO: 处理单元测试与任务
+	Cli::run();
+}
+else // web
+{
+	//处理请求
+	Server::run();
+}
