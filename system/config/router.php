c342d2d60793c7d9065c8f5e14ab8a0e338d1a9a
shijianhang <3076679995@qq.com>
2016-10-09 10:56:28 +0800

    改进路由

--- system/config/router.php
+++ system/config/router.php
@@ -2,23 +2,18 @@
 
 // 路由规则配置
 return array(
-	// 1 自带参数的正则
-	'/(?P<controller>.+)\/(?P<action>.+)\/(?P<id>\d+)/',
+	// 1 自带参数的正则 + 预设参数
+	'/(?P<controller>.+)\/(?P<action>.+)\/(?P<id>\d+)/' => array(
+		'controller' => 'Home',
+		'action' => 'index',
+	),
 	
 	// 2 正则 + 参数映射函数（从正则匹配结果中获得参数）
-	'/(.+)\/(.+)\/(\d+)/' => function($mathes){
+	'/(.+)\/(.+)(\/(\d+))?/' => function($mathes){
 		return array(
 			'controller' => $mathes[1],
 			'action' => $mathes[2],
 			'id' => $mathes[3],
-		);
-	},
-	
-	// 3 正则 + 预设参数
-	'/.+/' => function($mathes){
-		return array(
-			'controller' => 'Home',
-			'action' => 'index',
 		);
 	},
 );
