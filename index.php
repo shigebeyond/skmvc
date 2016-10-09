ebdd6782b9819346ab6ba34967fdbfc40c740b11
shijianhang <3076679995@qq.com>
2016-10-09 10:56:07 +0800

    添加debug的环境变量

--- index.php
+++ index.php
@@ -1,9 +1,19 @@
 <?php
 
+// 定义环境常量
+define('ENV', 'debug');
+define('DEBUG', Env == 'debug');
+
 // 定义相关目录的常量
 define('ROOTPATH', dirname(__FILE__).DIRECTORY_SEPARATOR);
 define('APPPATH', ROOTPATH.'application'.DIRECTORY_SEPARATOR);
 define('SYSPATH', ROOTPATH.'system'.DIRECTORY_SEPARATOR);
+
+// 配置错误输出
+if(DEBUG)
+	error_reporting(E_ALL | E_STRICT);
+else 
+	error_reporting(E_ALL ^ E_NOTICE);
 
 // 自动加载
 require SYSPATH.'classes/Sk/Loader.php';
@@ -12,7 +22,7 @@ spl_autoload_register(array('Sk_Loader', 'load_class'));
 if (PHP_SAPI == 'cli') // 命令行
 {
 	// TODO: 处理单元测试与任务
-	Cli::run();
+	//Cli::run();
 }
 else // web
 {
