d114fd473b334b161cfdb94b1932375240a5280a
shijianhang <3076679995@qq.com>
2016-10-11 15:04:53 +0800

    完善注释

--- system/classes/Sk/Singleton/Configurable.php
+++ system/classes/Sk/Singleton/Configurable.php
@@ -12,52 +12,66 @@
  */
 class Sk_Singleton_Configurable
 {
-    protected static $_instances = array();
-
-    /**
-     * 根据不同的配置来获得不同的实例
-     * 
-     * @param string $group
-     * @throws Exception
-     * @return multitype
-     */
-    public static function instance($name = 'default')
-    {
-    	// 获得当前类
-    	$class = get_called_class();
-    	
-    	// 使用类名小写作为配置组名
-    	$group = strtolower($class);
-    	// 使用 $name 作为配置路径
-    	if($name)
-    		$group .= '.'.$name;
-    	
-    	// 缓存 or 加载
-        if (!isset(self::$_instances[$group])) {
-        	// 加载配置项
-        	$config = Config::load($group);
-        	
-        	// 获得子类后缀
-        	$subclass_suffix = static::subclass_suffix($config);
-        	// 获得子类 = 父类_子类后缀
-			if($subclass_suffix)
-				$class .= '_'.$subclass_suffix;
+	/**
+	 * 实例池: <$name, 实例>
+	 * @var array
+	 */
+	protected static $_instances = array ();
+	
+	/**
+	 * 根据不同的配置来获得不同的实例
+	 *
+	 * @param string $name	分类名, 可作为配置中的子路径
+	 * @throws Exception
+	 * @return multitype
+	 */
+	public static function instance($name = 'default') 
+	{
+		// 获得当前类
+		$class = get_called_class ();
+		
+		// 使用类名小写作为配置组名
+		$group = strtolower ( $class );
+		// 使用 $name 作为配置路径
+		if ($name)
+			$group .= '.' . $name;
 			
-        	// 根据配置项来创建实例
-            self::$_instances = new $class($config, $name);
-        }
-
-        return self::$_instances;
-    }
-    
-    /**
-     * 根据配置来获得子类后缀
-     * 
-     * @param array $config
-     * @return string
-     */
-    public static function subclass_suffix($config)
-    {
-    	return NULL;
-    }
+			// 缓存 or 加载
+		if (! isset ( self::$_instances [$group] )) {
+			// 加载配置项
+			$config = Config::load ( $group );
+			
+			// 获得子类后缀
+			$subclass_suffix = static::subclass_suffix ( $config );
+			// 获得子类 = 父类_子类后缀
+			if ($subclass_suffix)
+				$class .= '_' . $subclass_suffix;
+				
+				// 根据配置项来创建实例
+			self::$_instances = new $class ( $config, $name );
+		}
+		
+		return self::$_instances;
+	}
+	
+	/**
+	 * 根据配置来获得子类后缀
+	 *
+	 * @param array $config        	
+	 * @return string
+	 */
+	public static function subclass_suffix($config) 
+	{
+		return NULL;
+	}
+	
+	/**
+	 * 强制子类要处理接收到的两个参数
+	 *
+	 * @param array $config 	对应的配置项
+	 * @param string $name	分类名
+	 */
+	public function __construct($config, $name = NULL) 
+	{
+	}
 }
