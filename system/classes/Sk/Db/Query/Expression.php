64b437460e69f10aa09ca649210c19a032453964
shijianhang <3076679995@qq.com>
2016-10-12 18:56:48 +0800

    添加备忘

--- system/classes/Sk/Db/Query/Expression.php
+++ system/classes/Sk/Db/Query/Expression.php
@@ -10,94 +10,58 @@
  */
 class Sk_Db_Query_Expression
 {
-	public static $config = array(
-		'select' => [','],
-		'from' => NULL,
-		'where' => [],
-		'limit' => [' '],
-		'order_by' => [','],
-		'group_by' => [','],
-		'where_group' => ['AND', '(', ')'],
-		'or_where_group' => ['AND', '(', ')'],
-		'having' => ['AND', '(', ')'],
-	);
-	
-	public static function instance($type)
-	{
-		$arr = array();
-		$arr['select'] = new static(',');
-		$arr['from'] = new static();
-		
-	}
-	
 	/**
-	 * 分割符
+	 * 主语
 	 * @var string
 	 */
-	protected $_delimiter;
+	protected $_subject;
 	
 	/**
-	 * 头部
+	 * 谓语
 	 * @var string
 	 */
-	protected $_head;
+	protected $_predicate; 
+	 
+	 /**
+	  * 宾语
+	  * @var string
+	  */
+	protected $_object;
 	
 	/**
-	 * 尾部
-	 * @var string
-	 */
-	protected $_tail;
-	
-	
-	public function __construct($delimiter, $head = NULL, $tail = NULL)
-	{
-		$this->_delimiter = $delimiter;
-		$this->_head = $head;
-		$this->_tail = $tail;
-	}
-	
-	/**
-	 * 生成主谓宾表达式
-	 * 
-	 * @param string $predicate 谓语
-	 * @param string $object 宾语
-	 * @param string $subject 主语
+	 * 构建主谓宾表达式
+	 *
 	 * @return string
 	 */
-	public function build($predicate, $object, $subject = NULL)
+	public function build()
 	{
-		// 谓语 + 宾语
-		$exp = $predicate.' '.$object;
+		// 构建多值宾语
+		if(is_array($this->_object))
+			$object = $this->build_object($this->_object);
 		
-		// 主语
-		if ($subject) 
-			$exp = $subject.' '.$exp;
-		
+		// 主谓宾
+		return trim("$this->_subject $this->_predicate $object");
+	}
+	
+	/**
+	 * 构建多值宾语
+	 * 
+	 * @param array $object
+	 * @return string
+	 */
+	public function build_object(array $object, $delimiter = ',', $head = '(', $tail = ')')
+	{
+		// 分隔符
+		$exp = implode($delimiter, $object);
+			
+		// 头部
+		if($head)
+			$exp = $head.$exp;
+			
+		// 尾部
+		if($tail)
+			$exp .= $tail;
+			
 		return $exp;
-	}
-	
-	/**
-	 * 生成宾语表达式
-	 * 
-	 * @param unknown $object
-	 * @return string
-	 */
-	public function build_object($object)
-	{
-		// 对数组类型
-		if(is_array($object))
-		{
-			$exp = implode($this->_delimiter, $object);
-			
-			if($this->_head)
-				$exp = $this->_head.$exp;
-			
-			if($this->_tail)
-				$exp .= $this->_tail;
-			
-			return $exp;
-		}
-		
-		return $object;
 	}
 }
