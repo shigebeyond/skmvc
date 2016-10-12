887811a5aa34c2f38a7d16ae82b79dc2286812b3
shijianhang <3076679995@qq.com>
2016-10-12 18:56:30 +0800

    删除无用代码

--- system/classes/Db/Query/Builder.php
+++
@@ -1,3 +0,0 @@
-<?php defined('SYSPATH') OR die('No direct script access.');
-
-class Db_Query_Builder extends Sk_Db_Query_Builder{}
--- system/classes/Sk/Db/Query/Builder.php
+++
@@ -1,6 +0,0 @@
-<?php defined('SYSPATH') OR die('No direct script access.');
-
-class Sk_Db_Query_Builder
-{
-	
-}
