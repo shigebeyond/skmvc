bb84f5c3a471ed69717806529a2b1386f97e4eac
shijianhang <3076679995@qq.com>
2016-10-14 12:55:35 +0800

    fix little bug

--- system/classes/Sk/Db/Query/Builder/Decoration/Expression/Simple.php
+++ system/classes/Sk/Db/Query/Builder/Decoration/Expression/Simple.php
@@ -22,7 +22,11 @@ class Sk_Db_Query_Builder_Decoratoin_Expression_Simple extends Db_Query_Builder_
     {
         // 将连接符也记录到子表达式中, 忽略第一个子表达式的连接符 => 编译好子表达式直接拼接就行
         if(!empty($this->_subexps))
-            $subexp[] = $delimiter;
+        {
+            // 有可能子表达式的size < 元素处理器的size(如limit表达式), 因此以元素处理器的size为准
+            $size = count($this->_element_handlers);
+            $subexp[$size] = $delimiter;
+        }
 
         $this->_subexps[] = $subexp;
         return $this;
@@ -39,7 +43,8 @@ class Sk_Db_Query_Builder_Decoratoin_Expression_Simple extends Db_Query_Builder_
         foreach ($this->_element_handlers as $i => $handler)
         {
             // 处理某个元素的值
-            $subexp[$i] = $this->{"_$handler"}($subexp[$i]);
+            $value = Arr::get($subexp, $i); // 有可能子表达式的size < 元素处理器的size(如limit表达式)
+            $subexp[$i] = $this->{"_$handler"}($value);
         }
 
         return implode(' ', $subexp); // 用空格拼接多个元素
