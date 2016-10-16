cacc47f9ddeb700ce3503f9abb263a2c21570b24
shijianhang <3076679995@qq.com>
2016-10-16 20:46:05 +0800

    用单独的query builder子类来替换orm父类orm_finder

---
+++ system/classes/Orm/Query/Builder.php
@@ -0,0 +1,12 @@
+<?php defined('SYSPATH') OR die('No direct script access.');
+
+/**
+ * 面向orm对象的sql构建器
+ * 
+ * @Package package_name 
+ * @category 
+ * @author shijianhang
+ * @date 2016-10-16 下午8:02:22 
+ *
+ */
+class Orm_Query_Builder extends Sk_Orm_Query_Builder {}
