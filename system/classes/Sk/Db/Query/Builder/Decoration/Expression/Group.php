3b005b6635d7ffabb1a89c14a7fef2e864a47e39
shijianhang <3076679995@qq.com>
2016-10-17 08:42:06 +0800

    格式化代码

--- system/classes/Sk/Db/Query/Builder/Decoration/Expression/Group.php
+++ system/classes/Sk/Db/Query/Builder/Decoration/Expression/Group.php
@@ -11,67 +11,72 @@
  */
 class Sk_Db_Query_Builder_Decoration_Expression_Group extends Db_Query_Builder_Decoration_Expression
 {
-    /**
-     * 开启一个分组
-     * @param $delimiter
-     * @return Sk_Db_Query_Builder_Decoration_Expression_Group
-     */
-    public function open($delimiter)
-    {
-        // 将连接符也记录到子表达式中, 忽略第一个子表达式
-        $subexp = '(';
-        if (!empty($this->_subexps))
-            $subexp = $delimiter.$subexp;
-
-        $this->_subexps[] = $subexp;
-        return $this;
-    }
-
-    /**
-     * 结束一个分组
-     * @return Sk_Db_Query_Builder_Decoration_Expression_Group
-     */
-    public function close()
-    {
-        $this->_subexps[] = ')';
-        return $this;
-    }
-
-    /**
-     * 获得最后一个子表达式
-     * @return Db_Query_Builder_Decoration_Expression
-     */
-    public function end_subexp()
-    {
-        $last = end($this->_subexps);
-		if(!$last instanceof Sk_Db_Query_Builder_Decoration_Expression_Simple)
-            $this->_subexps[] = $last = new Sk_Db_Query_Builder_Decoration_Expression_Simple($this->_db, NULL, $this->_element_handlers);
-        return $last;
-    }
-
-    /**
-     * 添加子表达式
-     * @param array $subexp
-     * @param string $delimiter
-     * @return Sk_Db_Query_Builder_Decoration_Expression_Group
-     */
-    public function add_subexp(array $subexp, $delimiter = ', ')
-    {
-        // 代理最后一个子表达式
-        $this->end_subexp()->add_subexp($subexp, $delimiter);
-        return $this;
-    }
-
-    /**
-     * 编译一个子表达式
-     * @param array $subexp
-     * @return string
-     */
-    public function compile_subexp($subexp)
-    {
-        // 子表达式是: string / Sk_Db_Query_Builder_Decoration_Expression_Simple
-        // Sk_Db_Query_Builder_Decoration_Expression_Simple 转字符串自动compile
-        return "$subexp";
-    }
+	/**
+	 * 开启一个分组
+	 * 
+	 * @param	$delimiter
+	 * @return Sk_Db_Query_Builder_Decoration_Expression_Group
+	 */
+	public function open($delimiter) 
+	{
+		// 将连接符也记录到子表达式中, 忽略第一个子表达式
+		$subexp = '(';
+		if (! empty ( $this->_subexps ))
+			$subexp = $delimiter . $subexp;
+		
+		$this->_subexps [] = $subexp;
+		return $this;
+	}
+	
+	/**
+	 * 结束一个分组
+	 * 
+	 * @return Sk_Db_Query_Builder_Decoration_Expression_Group
+	 */
+	public function close() 
+	{
+		$this->_subexps [] = ')';
+		return $this;
+	}
+	
+	/**
+	 * 获得最后一个子表达式
+	 * 
+	 * @return Db_Query_Builder_Decoration_Expression
+	 */
+	public function end_subexp() 
+	{
+		$last = end ( $this->_subexps );
+		if (! $last instanceof Sk_Db_Query_Builder_Decoration_Expression_Simple)
+			$this->_subexps [] = $last = new Sk_Db_Query_Builder_Decoration_Expression_Simple ( $this->_db, NULL, $this->_element_handlers );
+		return $last;
+	}
+	
+	/**
+	 * 添加子表达式
+	 * 
+	 * @param array $subexp        	
+	 * @param string $delimiter        	
+	 * @return Sk_Db_Query_Builder_Decoration_Expression_Group
+	 */
+	public function add_subexp(array $subexp, $delimiter = ', ') 
+	{
+		// 代理最后一个子表达式
+		$this->end_subexp ()->add_subexp ( $subexp, $delimiter );
+		return $this;
+	}
+	
+	/**
+	 * 编译一个子表达式
+	 * 
+	 * @param array $subexp        	
+	 * @return string
+	 */
+	public function compile_subexp($subexp) 
+	{
+		// 子表达式是: string / Sk_Db_Query_Builder_Decoration_Expression_Simple
+		// Sk_Db_Query_Builder_Decoration_Expression_Simple 转字符串自动compile
+		return "$subexp";
+	}
 
 }
--- system/classes/Sk/Db/Query/Builder/Decoration/Expression/Simple.php
+++ system/classes/Sk/Db/Query/Builder/Decoration/Expression/Simple.php
@@ -15,43 +15,45 @@
  */
 class Sk_Db_Query_Builder_Decoration_Expression_Simple extends Db_Query_Builder_Decoration_Expression
 {
-    /**
-     * 添加一个子表达式+连接符
-     *
-     * @param array $subexp 子表达式
-     * @param string $delimiter 当前子表达式的连接符
-     * @return Sk_Db_Query_Builder_Expression
-     */
-    public function add_subexp(array $subexp, $delimiter = ', ')
-    {
-        // 将连接符也记录到子表达式中, 忽略第一个子表达式的连接符 => 编译好子表达式直接拼接就行
-        if(!empty($this->_subexps))
-        {
-            // 有可能子表达式的size < 元素处理器的size(如limit表达式), 因此以元素处理器的size为准
-            $size = count($this->_element_handlers);
-            $subexp[$size] = $delimiter;
-        }
-
-        $this->_subexps[] = $subexp;
-        return $this;
-    }
-
-    /**
-     * 编译一个子表达式
-     * @param array $subexp
-     * @return string
-     */
-    public function compile_subexp($subexp)
-    {
-        // 遍历处理器来处理对应元素, 没有处理的元素也直接拼接
-        foreach ($this->_element_handlers as $i => $handler)
-        {
-            // 处理某个元素的值
-            $value = Arr::get($subexp, $i); // 有可能子表达式的size < 元素处理器的size(如limit表达式)
-            $subexp[$i] = $this->{"_$handler"}($value);
-        }
-
-        return implode(' ', $subexp); // 用空格拼接多个元素
-    }
+	/**
+	 * 添加一个子表达式+连接符
+	 *
+	 * @param array $subexp
+	 *        	子表达式
+	 * @param string $delimiter
+	 *        	当前子表达式的连接符
+	 * @return Sk_Db_Query_Builder_Expression
+	 */
+	public function add_subexp(array $subexp, $delimiter = ', ') {
+		// 将连接符也记录到子表达式中, 忽略第一个子表达式的连接符 => 编译好子表达式直接拼接就行
+		if (! empty ( $this->_subexps )) 
+		{
+			// 有可能子表达式的size < 元素处理器的size(如limit表达式), 因此以元素处理器的size为准
+			$size = count ( $this->_element_handlers );
+			$subexp [$size] = $delimiter;
+		}
+		
+		$this->_subexps [] = $subexp;
+		return $this;
+	}
+	
+	/**
+	 * 编译一个子表达式
+	 * 
+	 * @param array $subexp        	
+	 * @return string
+	 */
+	public function compile_subexp($subexp) 
+	{
+		// 遍历处理器来处理对应元素, 没有处理的元素也直接拼接
+		foreach ( $this->_element_handlers as $i => $handler ) 
+		{
+			// 处理某个元素的值
+			$value = Arr::get ( $subexp, $i ); // 有可能子表达式的size < 元素处理器的size(如limit表达式)
+			$subexp [$i] = $this->{"_$handler"} ( $value );
+		}
+		
+		return implode ( ' ', $subexp ); // 用空格拼接多个元素
+	}
 
 }
