b1c383cd0bc7b7cea5d7a2be7dbccf51eb93df5b
shijianhang <3076679995@qq.com>
2016-10-29 19:10:12 +0800

    添加ｍｏｎｇｏ配置

---
+++ system/config/mongoo.php
@@ -0,0 +1,14 @@
+<?php defined('SYSPATH') OR die('No direct script access.');
+
+return array(
+    'default' => array(
+		'server'	=> 'mongodb://localhost:27017',
+        'options'   => array(
+        		'db' => 'test',
+        		// 'username' => 'test',　//　用户名
+        		// 'password' => 'test', //　密码
+        		//　'persist' => 'p', //　长连接的标识
+        		// 'replicaSet' => 'myReplSet',　//　
+	    ),
+    ),
+);
