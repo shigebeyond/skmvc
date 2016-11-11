527e60c550ed73e0d49fd022e8bf07cc9a5ad16b
shijianhang <3076679995@qq.com>
2016-11-11 17:53:23 +0800

    fix bug: 输出mongo命令出错

--- system/classes/Sk/Mongoo/Query/Builder.php
+++ system/classes/Sk/Mongoo/Query/Builder.php
@@ -361,6 +361,8 @@ class Sk_Mongoo_Query_Builder
 				
 			return $mathes[0];
 		}, $command);
+		// 将[] 转为{}
+		$command = str_replace('[]', '{}', $command);
 		//　删除多余代码
 		return preg_replace('/\.limit\(0\)|\.skip\(0\)|\.sort\(\[\]\)|.count\(:apply_skip_limit\)/', '', $command);
 	}
